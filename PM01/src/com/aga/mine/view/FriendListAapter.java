da3b42c36db7a96c669af3fd93eef3e710c88721
eastflag <eastflag@eastflag-PC>
2014-04-08 01:09:48 +0900

    친구 프로필 사진 크기 조정 아이콘 금은동 처리 => 친구가 없어서 테스트는 못해봄.

--- PM01/src/com/aga/mine/view/FriendListAapter.java
+++ PM01/src/com/aga/mine/view/FriendListAapter.java
@@ -53,6 +53,7 @@ public class FriendListAapter extends BaseAdapter {
 		if(convertView == null) {
 			holder = new Viewholder();
 			convertView = View.inflate(mContext, R.layout.list_friends, null);
+			holder.imgNumber = (ImageView) convertView.findViewById(R.id.imgNumber);
 			holder.imgProfile = (ImageView) convertView.findViewById(R.id.imgProfile);
 			holder.imgGift = (ImageView) convertView.findViewById(R.id.imgGift);
 			holder.imgBroomstick = (ImageView) convertView.findViewById(R.id.imgBroomstick);
@@ -64,25 +65,50 @@ public class FriendListAapter extends BaseAdapter {
 			holder = (Viewholder) convertView.getTag();
 		}
 		
-		if (gameScore.get(position).id.equals(user.getId())) {
-			holder.imgGift.setVisibility(View.GONE);
-			holder.imgBroomstick.setVisibility(View.GONE);
+		//1-3등: 금은동 아이콘, 4등~ : no image 처리
+		switch(position) {
+		case 0: 
+			holder.imgNumber.setImageResource(R.drawable.home_circlegold_hd);
+			break;
+		case 1:
+			holder.imgNumber.setImageResource(R.drawable.home_circlesilver_hd);
+			break;
+		case 2:
+			holder.imgNumber.setImageResource(R.drawable.home_circlebronze_hd);
+			break;
+		default:
+			holder.imgNumber.setVisibility(View.INVISIBLE);
+			break;
 		}
+		
+		if (gameScore.get(position).id.equals(user.getId())) {
+			holder.imgGift.setVisibility(View.INVISIBLE);
+			holder.imgBroomstick.setVisibility(View.INVISIBLE);
+		}
+		
 		holder.tvRanking.setText(String.valueOf(position+1));
 		holder.tvFBId.setText(gameScore.get(position).name);
-//		holder.tvFBId.setText(friends.get(position).getName());
 		holder.tvScore.setText(String.valueOf(gameScore.get(position).score));
+		
+		//선물상자 클릭 이벤트 처리
+		holder.imgGift.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//To Do
+			}
+		});
+		
+		//빗자루 클릭 이벤트 처리
+		holder.imgBroomstick.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//To Do
+			}
+		});
 		
 		AQuery aq = mAq.recycle(convertView);
 		
-//		String url = "https://graph.facebook.com/" + friends.get(position).getId() +"/picture";
 		String url = "https://graph.facebook.com/" + gameScore.get(position).id +"/picture";
-//		Bitmap placeholder = null;
-//		try {
-//			placeholder = BitmapFactory.decodeStream( mContext.getAssets().open(""));
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
 
 		if(aq.shouldDelay(position, convertView, parent, url)){
 			//aq.id(holder.imgProfile).image(placeholder);
@@ -95,6 +121,7 @@ public class FriendListAapter extends BaseAdapter {
 	}
 	
 	class Viewholder {
+		ImageView imgNumber;
 		ImageView imgProfile;
 		ImageView imgGift;
 		ImageView imgBroomstick;
