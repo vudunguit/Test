f01c3825dd490659d86481aea20b701551d1cb68
seunghwannoh <sakuranox@gmail.com>
2014-04-11 18:32:24 +0900

    04/11 이모티콘샵 - 백보드 위치 안맞던것 수정 불필요한 이미지 삭제

--- PM01/src/com/aga/mine/mains/ShopEmoticon.java
+++ PM01/src/com/aga/mine/mains/ShopEmoticon.java
@@ -7,77 +7,45 @@ import org.cocos2d.nodes.CCLabel;
 import org.cocos2d.nodes.CCNode;
 import org.cocos2d.nodes.CCSprite;
 import org.cocos2d.types.CGPoint;
-import org.cocos2d.types.CGSize;
-
-import android.content.Context;
-import android.util.Log;
-import android.view.MotionEvent;
-
-import com.aga.mine.pages.UserData;
 
 public class ShopEmoticon extends CCLayer {
-
+	
 	final String commonfolder = "00common/";
 	final String folder = "24emoticon/";
 	final String fileExtension = ".png";
-
+	
 	CCSprite bg;
 	
 	int mode = 0;
 	// config 파일에 나중에 옮길것
 	public static boolean buttonActive = true;
-
 	
 	CCSprite boardFrame;
 	CCSprite 	layer4;
-
-	static CCLayer layer2;
-	private Context mContext;
-	private UserData userData;
 	
 	static CCScene scene() {
 		CCScene scene = CCScene.node();
 		CCLayer layer = new ShopEmoticon();
 		scene.addChild(layer);
-		layer2 = CCLayer.node();
-		scene.addChild(layer2);
-//		scene.addChild(InvitationReceiver.getInstance().getInvitationPopup());
 		return scene;
 	}
-
-//	private CGSize winsize() {
-//		return CCDirector.sharedDirector().winSize();
-//	}
-	
+		
 	public ShopEmoticon() {
-//		mContext = CCDirector.sharedDirector().getActivity();
-//		userData = UserData.share(mContext);
-//		CCTouchDispatcher.sharedDispatcher().addTargetedDelegate(this, 0, true);
-//		this.setIsTouchEnabled(true);
-//		setBackground();
-//		setBackBoardMenu();
-//		setForeground();
-		
-		mContext = CCDirector.sharedDirector().getActivity();
-		userData = UserData.share(mContext);
-		
 		bg = BackGround.setBackground(this, CGPoint.make(0.5f, 0.5f), commonfolder + "bg1" + fileExtension);
-//		setBackBoardMenu(commonfolder + "gamebb" + fileExtension);
 		setBackBoardMenu(commonfolder + "bb1" + fileExtension);
 		setBoardFrameMenu(commonfolder + "frameGeneral-hd" + fileExtension);
+		
 		TopMenu2.setSceneMenu(this);
 		BottomImage.setBottomImage(this);
-//		this.setIsTouchEnabled(true);
+
 		MainApplication.getInstance().getActivity().mHandler.sendEmptyMessage(Constant.MSG_DISPLAY_EMOTICONLIST);
 	}
 	
 	private void setBackBoardMenu(String imageFullPath) {
 		CCSprite bb = CCSprite.sprite(imageFullPath);
 		bg.addChild(bb);
-//		bb.setPosition(bg.getContentSize().width / 2, bg.getContentSize().height * 0.525f);
-		bb.setPosition(bg.getContentSize().width / 2, bg.getContentSize().height / 2);
+		bb.setPosition(bg.getContentSize().width / 2, bg.getContentSize().height * 0.525f);
 		bb.setAnchorPoint(0.5f, 0.5f);
-		setMainMenu(bb);
 	}
 	
 	private void setBoardFrameMenu(String imageFullPath) {
@@ -86,24 +54,6 @@ public class ShopEmoticon extends CCLayer {
 		boardFrame.setPosition(bg.getContentSize().width / 2, bg.getContentSize().height * 0.525f);
 		boardFrame.setAnchorPoint(0.5f, 0.5f);
 		FrameTitle6.setTitle(boardFrame, folder);
-	}
-	
-	private void setMainMenu(CCSprite parentSprite){
-		emoti(parentSprite);
-	}
-	
-	private void emoti(CCSprite parentSprite) {
-		String emoticonBundle = FacebookData.getinstance().getDBData("Emoticons");
-		String[] emoticonArray = emoticonBundle.split(",");
-		CGPoint point = CGPoint.ccp(
-				parentSprite.getPosition().x - parentSprite.getContentSize().width * (0.5f - parentSprite.getAnchorPoint().x), 
-				parentSprite.getPosition().y + parentSprite.getContentSize().height * (1 - parentSprite.getAnchorPoint().y) - 200);
-		for (String string : emoticonArray) {
-			CCLabel emoticon = CCLabel.makeLabel(string, "Arial", 20);
-			parentSprite.addChild(emoticon);
-			parentSprite.setPosition(point);
-			point.set(parentSprite.getPosition().x, parentSprite.getPosition().y - emoticon.getContentSize().height);
-		}
 	}
 	
 	final int previous = 501;
