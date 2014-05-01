756d3775a406a7101a77cce118b4b0f97086d355
eastflag <eastflag@eastflag-PC>
2014-05-01 11:10:56 +0900

    5/1 게임 로딩 바 애니메이션 구현
    그러나, Game.java를 로딩하면서 GameLoading.java 화면이 멈추기 때문에
    로딩 애니메이션이 중간에 멈추는 현상이 생김
    이걸 개선하기 위해서 Game.java 리소스를 별도의 싱글턴 인스턴스에
    로딩해야 함.

--- PM01/src/com/aga/mine/mains/GameLoading.java
+++ PM01/src/com/aga/mine/mains/GameLoading.java
@@ -1,5 +1,6 @@
 ﻿package com.aga.mine.mains;
 
+import org.cocos2d.actions.interval.CCScaleTo;
 import org.cocos2d.layers.CCLayer;
 import org.cocos2d.layers.CCScene;
 import org.cocos2d.nodes.CCDirector;
@@ -7,12 +8,17 @@ import org.cocos2d.nodes.CCSprite;
 import org.cocos2d.types.CGPoint;
 import org.cocos2d.types.CGSize;
 
+import android.util.Log;
+
 import com.aga.mine.pages2.Game;
 import com.aga.mine.util.Util;
 
 public class GameLoading extends CCLayer {
 	
 	CCSprite bg;
+	CCSprite bar;
+	CCSprite wizard;
+	float mScaleX = 1.0f;
 	
 	final String folder = "59gameload/";
 	final String fileExtension = ".png";
@@ -74,18 +80,41 @@ public class GameLoading extends CCLayer {
 		cloud.setAnchorPoint(0, 0);
 		bg.addChild(cloud);
 		
-		CCSprite bar = CCSprite.sprite(
+		bar = CCSprite.sprite(
 				folder + "loadpiece" + fileExtension);
 		bar.setPosition(160,127);
 		bar.setAnchorPoint(0, 0);
 		bg.addChild(bar);
-		bar.setScaleX(75);
+		//bar.setScaleX(75);
 		
-		CCSprite wizard = CCSprite.sprite(
-				folder + "loadwizard" + fileExtension);
-		wizard.setPosition(420,108);
+		wizard = CCSprite.sprite(folder + "loadwizard" + fileExtension);
+		wizard.setPosition(bar.getPosition().x + bar.getContentSize().width * bar.getScaleX() - wizard.getContentSize().width/2, bar.getPosition().y);
 		wizard.setAnchorPoint(0, 0);
 		bg.addChild(wizard);
+		
+//		CCScaleTo scale = CCScaleTo.action(3, 85, 1);
+//		bar.runAction(scale);
+		schedule("update");
+	}
+	
+	/**
+	 * GameLoading은 실제 1-2초가 소요되며 Game.java를 로딩하면서 GameLoading 화면이 멈춤. 
+	 * 이걸 개선하기 위해서는 Game.java에 소요되는 리소스를 Game 싱글턴 인스턴스에 로딩하고 끝나면 초기화하는
+	 * 루틴으로 가야함.
+	 */
+	public void update(float dt) {
+		mScaleX += dt*30;
+		bar.setScaleX(mScaleX);
+		
+		float barRight = bar.getPosition().x + bar.getContentSize().width * bar.getScaleX();
+		float limit = bg.getContentSize().width - bar.getPosition().x;
+		Log.d("LDK", "limit:" + limit);
+		
+		if(barRight > limit) {
+			unschedule("update");
+			Log.d("LDK", "update");
+		}
+		wizard.setPosition(barRight - wizard.getContentSize().width/2, bar.getPosition().y);
 	}
 	
 	public void nextSceneCallback(float dt) {
