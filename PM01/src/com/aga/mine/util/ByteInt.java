4c987b173b96270cf42d488e7a9e43521581a024
seunghwannoh <sakuranox@gmail.com>
2014-05-08 20:53:24 +0900

    05/08 추가

---
+++ PM01/src/com/aga/mine/util/ByteInt.java
@@ -0,0 +1,23 @@
+package com.aga.mine.util;
+
+public class ByteInt {
+	byte playType;
+	int data;
+
+	public byte getPlayType() {
+		return playType;
+	}
+
+	public void setPlayType(byte playType) {
+		this.playType = playType;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+}
