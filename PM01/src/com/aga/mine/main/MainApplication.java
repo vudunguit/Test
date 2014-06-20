180dcbd25d363934d62c1014b83751cc39fc4dea
eastflag <eastflag@192.168.0.224>
2014-06-20 09:02:56 +0900

    6/20 pumpkin 페이스북 app id가 설정되지 않아 임시로 제가 페이스북에 갖고 있는 타이핑게임 app id를 사용하고 있음.

--- PM01/src/com/aga/mine/main/MainApplication.java
+++ PM01/src/com/aga/mine/main/MainApplication.java
@@ -11,10 +11,10 @@ import android.os.Handler;
 
 public class MainApplication extends Application {
     //private static final String APP_ID = "625994234086470";
-//    private static final String APP_ID = "1387999011424838";
-//    private static final String APP_NAMESPACE = "sromkuapp";
-    private static final String APP_ID = "709362365791816";
-    private static final String APP_NAMESPACE = "Pumpkin Mine";
+	private static final String APP_ID = "1387999011424838";
+	private static final String APP_NAMESPACE = "typing_game";
+    //private static final String APP_ID = "709362365791816";
+    //private static final String APP_NAMESPACE = "pumpkinmine;
     
     private Context mContext;
     private MainActivity mActivity;
